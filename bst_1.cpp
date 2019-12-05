3cb83afbe5e2c9ef5e9c32be7d86ba59dbc537d4
tymc8009 <58447798+tymc8009@users.noreply.github.com>
2019-12-04 19:16:47 -0700

    Update bst_1.cpp

--- bst_1.cpp
+++ bst_1.cpp
@@ -4,6 +4,68 @@
 using namespace std;
 
 
+node* BST::getMinValueNode(node* currNode){
+
+    if(currNode->left == NULL){
+      return currNode;
+    }
+    return getMinValueNode(currNode->left);
+}
+
+
+void deleteItemHelper(node* currNode, int key){
+  if(currNode == NULL)
+  {
+    return NULL;
+  }
+  else if(key < currNode->key)
+  {
+    currNode->left = deleteItemHelper(currNode->left, key);
+  }
+  else if(key > currNode->key)
+  {
+    currNode->right = deleteItemHelper(currNode->right, key);
+  }
+  else{
+    //TODO Case : No child
+    if(currNode->left == NULL && currNode->right == NULL)
+    {
+        return NULL;
+    }
+    //TODO Case : Only right child
+    else if(currNode->left == NULL)
+    {
+      node *temp = currNode->right;
+      free(currNode);
+      return temp;
+    }
+    //TODO Case : Only left child
+    else if(currNode->right == NULL)
+    {
+      node *temp = currNode->left;
+      free(currNode);
+      return temp;
+    }
+    //TODO Case: Both left and right child
+    else
+    {
+      ///Replace with Minimum from right subtree
+      node* temp = getMinValueNode(currNode->right);
+      root->key = temp->key;
+      root->right = deleteNode(currNode->right, temp->key);
+    }
+  }
+}
+
+node* searchHelper(node* currNode, int key){
+  if(currNode){
+    if(currNode->key == key){
+      return currNode;
+    }
+    searchHelper(currNode->left, key);
+    searchHelper(currnode->right, key);
+  }
+}
 
 node* createNode(int key){
   node* newNode = new Node;
@@ -14,27 +76,45 @@ node* createNode(int key){
 }
 
 node* insertHelper(node* currNode, int key){
-
+  if(currNode == NULL){
+    return createNode(key);
+  }
+  else if(currNode->key > key){
+    insertHelper(currNode->left, key);
+  }
+  else if(currNode->key < key){
+    insertHelper(currNode->right, key);
+  }
+  return currNode;
 }
 
 HashTable(int bsize){
-
+  this->tableSize = bsize;
+  table = new node*[tableSize];
+  for(int i=0;i<bsize;i++)
+      table[i] = nullptr;
 }
+
 ~hashTable(){
 
 }
+
 unsigned int HashTable::hashFunction(int key){
   return (key % tableSize);
 }
+
 node* HashTable::searchItem(int key){
-
-}
-bool HashTable::insertItem(int key){
   int index = hashFunction(key);
-  
-
-
+  node* giveBack = searchHelper(table[index], key);
+  return giveBack;
 }
-void HashTable::deleteItem(int key){
+void HashTable::insertItem(int key){
+  int index = hashFunction(key);
+  node* root = table[index];
+  table[index] = insertHelper(root, key);
+}
 
+void HashTable::deleteItem(int key){
+  int index = hashFunction(key);
+  deleteItemHelper(table[index], key);
 }
