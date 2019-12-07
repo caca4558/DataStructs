90dae98f6ef3f40f1338338fa90b2a996f116a97
tymc8009 <58447798+tymc8009@users.noreply.github.com>
2019-12-07 16:28:32 -0700

    Update linProbing_1.cpp

--- linProbing_1.cpp
+++ linProbing_1.cpp
@@ -13,10 +13,14 @@ node* HashTable::createNode(int key)
 
 HashTable::HashTable(int bsize)
 {
-    this->tableSize= bsize;
+    this->tableSize = bsize;
     table = new node*[tableSize];
     for(int i=0;i<bsize;i++)
         table[i] = nullptr;
+}
+
+HashTable::~HashTable(){
+
 }
 
 unsigned int HashTable::hashFunction(int key)
@@ -28,12 +32,24 @@ node* HashTable::searchItem(int key)
 {
     int count = 0;
     int index = hashFunction(key);
-    while(count < table.size()){
-      if(table[index]->key == key){
-        return table[index];
+    while(count < tableSize){
+      if(table[index] != NULL){
+        if(table[index]->key == key){
+          return table[index];
+        }
+        else{
+          if(index == tableSize){
+            index = 0;
+            count++;
+          }
+          else{
+            index++;
+            count++;
+          }
+        }
       }
       else{
-        if(index == table.size()){
+        if(index == tableSize){
           index = 0;
           count++;
         }
@@ -43,11 +59,14 @@ node* HashTable::searchItem(int key)
         }
       }
     }
-    return NULL;
+  return NULL;
 }
 
 bool HashTable::insertItem(int key)
 {
+    if(searchItem(key) != NULL){
+      return false;
+    }
     int index = hashFunction(key);
     node* checker = table[index];
     if(table[index] == NULL){
@@ -64,7 +83,7 @@ bool HashTable::insertItem(int key)
           break;
         }
         else{
-          if(index == table.size()){
+          if(index == tableSize){
             index = 0;
           }
           else{
@@ -73,26 +92,30 @@ bool HashTable::insertItem(int key)
         }
       }
     }
+    return true;
 }
 
 void HashTable::deleteItem(int key){
   node* temp;
   int index = hashFunction(key);
   while(true){
+    if(table[index] == NULL){
+      return;
+    }
     if(table[index]->key == key){
       temp = table[index];
-      table[index]->key = NULL;
+      table[index] = NULL;
       delete temp;
       break;
     }
     else{
-      if(index == table.size){
+      if(index == tableSize){
         index = 0;
       }
       else{
         index++;
       }
     }
+
   }
 }
-
