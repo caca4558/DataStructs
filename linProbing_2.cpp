de1c75430e54202de724107bb75c4e248872246d
tymc8009 <58447798+tymc8009@users.noreply.github.com>
2019-12-08 15:51:16 -0700

    Update linProbing_2.cpp

--- linProbing_2.cpp
+++ linProbing_2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 #include "linProbing.hpp"
 
 
@@ -13,7 +14,7 @@ node* HashTable::createNode(int key)
 
 HashTable::HashTable(int bsize)
 {
-    this->tableSize= bsize;
+    this->tableSize = bsize;
     table = new node*[tableSize];
     for(int i=0;i<bsize;i++)
         table[i] = nullptr;
@@ -21,31 +22,98 @@ HashTable::HashTable(int bsize)
 
 unsigned int HashTable::hashFunction(int key)
 {
-    return (floor(key/tableSize)%tableSize);
-}
-
-void HashTable::deleteItem(int key){
-  int index = hashFunction(key);
-  node* temp = table[index];
-  delete(temp);
+  unsigned int val = floor(key/tableSize);
+  return (val%tableSize);
 }
 
 node* HashTable::searchItem(int key)
 {
+    int count = 0;
     int index = hashFunction(key);
-    return table[index];
+    while(count < tableSize){
+      if(table[index] != NULL){
+        if(table[index]->key == key){
+          return table[index];
+        }
+        else{
+          if(index == tableSize){
+            index = 0;
+            count++;
+          }
+          else{
+            index++;
+            count++;
+          }
+        }
+      }
+      else{
+        if(index == tableSize){
+          index = 0;
+          count++;
+        }
+        else{
+          index++;
+          count++;
+        }
+      }
+    }
+  return NULL;
 }
 
 bool HashTable::insertItem(int key)
 {
-    if(!searchItem(key))
-    {
-        int index = hashFunction(key);
-        table[index] = createNode(key,table[index]);
-        return true;
-     }
-    else{
-        cout<<"duplicate entry: "<<key<<endl;
-        return false;
+    if(searchItem(key) != NULL){
+      return false;
     }
+    int index = hashFunction(key);
+    node* checker = table[index];
+    if(table[index] == NULL){
+      table[index] = createNode(key);
+    }
+    // if(searchItem(key) != NULL){
+    //   return false;
+    // }
+    else{
+      bool check = false;
+      while(check == false){
+        if(table[index] == NULL){
+          table[index] = createNode(key);
+          break;
+        }
+        else{
+          if(index == tableSize){
+            index = 0;
+          }
+          else{
+            index++;
+          }
+        }
+      }
+    }
+    return true;
+}
+
+void HashTable::deleteItem(int key){
+  node* temp;
+  int index = hashFunction(key);
+  while(true){
+    if(table[index] == NULL){
+      return;
+    }
+    if(table[index]->key == key){
+      temp = table[index];
+      table[index] = NULL;
+      delete temp;
+      break;
+    }
+    else{
+      if(index == tableSize){
+        index = 0;
+      }
+      else{
+        index++;
+      }
+    }
+
+  }
 }
